23404ff30ef288b7a3908fa4d9cc4e9ef3767481
dexterhere04 <sdrtharun@gmail.com>
2026-01-12 21:53:22 +0530

    adding middleware

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -5,3 +5,10 @@ import (
 	chimiddle "github.com/go-chi/chi/middleware"
 	"github.com/dexterhere04/Learn_go/internal/middleware"
 )
+
+func Handler(r *chi.Mux){
+	r.use(chimiddle.StripSlashes)
+	r.Route("/account",func(router chi.Router)){
+		router.Use(middleware.Authorization)
+	}
+}
